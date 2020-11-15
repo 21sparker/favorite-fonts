9dd6d11a74524c112daa302cf18e06bf6c5028b4
21sparker <seanparker500@gmail.com>
2020-11-15 18:51:19 -0500

    Add font size skeleton

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -16,6 +16,15 @@ const Toolbar = () => {
                         placeholder='Type Something'
                         className={styles['sample-input']}/>
                 </div>
+                <div className={styles['font-size-container']}>
+                    <select className={styles['font-size-input']}>
+                        <option value="8">8px</option>
+                        <option value="12">12px</option>
+                        <option value="16">16px</option>
+                        <option value="24">24px</option>
+                        <option value="32">32px</option>
+                    </select>
+                </div>                
             </div>
         </div>
         
