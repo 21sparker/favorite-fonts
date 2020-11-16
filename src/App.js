719f985f7ce75274ac50498f0aac8f409331da0a
21sparker <seanparker500@gmail.com>
2020-11-15 22:18:44 -0500

    Add skeleton for remaining buttons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Toolbar from './components/Toolbar/Toolbar';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faList, faRedo } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faList, faRedo);
 
 function App() {
   return (
